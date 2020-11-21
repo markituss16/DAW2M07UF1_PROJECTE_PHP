e30807e63e70e82d94de8ace2437f2aac14b83c8
Marc <37379266+markituss16@users.noreply.github.com>
2020-11-21 23:07:56 +0100

    Update autenticacio.php

--- autenticacio.php
+++ autenticacio.php
@@ -2,20 +2,11 @@
 <head>
     <meta charset="utf-8">
     <title>Error d'autenticació</title>
-    <style>
-        body,html {
-            height: 100%;
-            margin: auto auto;
-            width: 100%;
-        }
-    </style>
 </head>
 
 <body>
-    <p>
-    No pots accedir a aquesta pàgina perquè encara no t'has autenticat.<br> 
-    Inicia sessió abans. 
-    </p>
+    <p>No pots accedir a aquesta pàgina perquè encara no t'has autenticat.<br> 
+    Inicia sessió abans</p>
     <a href="./login.html" class="button">Tornar a l'inici</a>
 </body>
 <html>
